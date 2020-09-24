8d6b5633920cf038e793bfd791d71df5152a7a0d
atikla <laithatik@gmail.com>
2020-09-25 00:56:31 +0300

    api\user\ user auth controller created

---
+++ app/Http/Controllers/API/user/auth/UserAuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\user\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    
+}
